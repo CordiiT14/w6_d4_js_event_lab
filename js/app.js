53198e0b4e61037f60cb7f9a1ca54350f136f1d0
CordiiT14 <cordelia.toennies@live.com>
2022-03-31 15:36:50 +0100

    latest changes - still not working

--- js/app.js
+++ js/app.js
@@ -6,9 +6,22 @@ form.addEventListener("submit", handleFormSubmit);
 
 })
 
+
+
+const readinglist = [];
+
 const handleFormSubmit = function(event){
   event.preventDefault();
-  const readingListItem = document.querySelector('#reading-list');
+  const readingListItem = document.querySelector('ul');
   readingListItem.textContent = `${event.target.title.value} ${event.target.author.value} ${event.target.category.value}`;
+  const newListItem = document.createElement('li');
+  newListItem.textContent = `${event.target.title.value} ${event.target.author.value} ${event.target.category.value}`;
+  const list = document.querySelector('ul');
+  list.appendChild(newListItem);
 };
+
+console.log(readinglist);
+
+
+
 
