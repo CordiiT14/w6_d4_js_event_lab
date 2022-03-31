1bbb0f336315402e01a5ced1424b85788e1dfaee
CordiiT14 <cordelia.toennies@live.com>
2022-03-31 14:46:11 +0100

    handleFormSubmit and displays input

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+const form = document.querySelector('#new-item-form');
+form.addEventListener("submit", handleFormSubmit);
+
 })
+
+const handleFormSubmit = function(event){
+  event.preventDefault();
+  const readingListItem = document.querySelector('#reading-list');
+  readingListItem.textContent = `${event.target.title.value} ${event.target.author.value} ${event.target.category.value}`;
+};
+
